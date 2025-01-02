199345dd1d55b60ba46e8a3b25fe28381a3c6cdc
danhales <danhalesprogramming@gmail.com>
2025-01-02 18:13:34 -0500

    added database command line option, renamed server_check to server_timestamp, added server timestamp in boilerplate comment to top of module

--- module-generators/neo4j/modulegenerator.py
+++ module-generators/neo4j/modulegenerator.py
@@ -29,13 +29,14 @@ path = f'{sys.path[-1].split(".env")[0]}module-generators/neo4j'
 sys.path.append(path)
 import modulegenerator
 
-graph = {
+profile = {
     'uri':'bolt://localhost:7687',
+    'database':'neo4j',
     'username':'neo4j',
     'password':'neo4j-dev'
 }
 
-def _authenticated_driver(uri=graph['uri'], username=graph['username'], password=graph['password']):
+def _authenticated_driver(uri=profile['uri'], username=profile['username'], password=profile['password']):
     """
     Internal method to set up an authenticated driver.
 
@@ -72,9 +73,10 @@ def _query(query_text=None, query_params=None):
     driver = _authenticated_driver()
     return driver.execute_query(query_text, query_params)
 
-def _server_check():
+def _server_timestamp():
     """
-    Retrieves a timestamp from the driver and prints a message to the screen.
+    Retrieves a timestamp from the neo4j isntance and prints a message 
+    to the screen.
 
     Parameters
     ----------
@@ -82,10 +84,10 @@ def _server_check():
 
     Returns
     -------
-    None
+    str:
+        Timestamp from server.
     """
-    timestamp = _query(query_text="RETURN localdatetime() AS `time`").records[0].get('time')
-    print(f'Server checked at: {timestamp}')
+    return _query(query_text="RETURN localdatetime() AS `time`").records[0].get('time')
 
 def _setup(output_directory=None):
     """
@@ -184,12 +186,16 @@ def generate_module(uri=None, username=None, password=None, graph=None, output_d
         _log(f'Old module found; deleting.')
         os.remove(filename)
 
+    # Boilerplate
+    _log('Writing boilerplate to top of file.')
+    _append(filename, f'# Date generated: {_server_timestamp()}\n')
+
     _log('Appending imports to module')
     _append_imports(filename, imports=['neo4j'])
 
     # get an authenticated driver
     driver = _authenticated_driver(uri=uri, username=username, password=password)
-    _server_check()
+    _server_timestamp()
 
     _log(f"{filename} successfully generated.")
     return filename
@@ -199,35 +205,38 @@ if __name__ == '__main__':
     
     # Configure command-line arguments.
     parser = argparse.ArgumentParser(description='Utility script for generating neo4j backend code.')
-    parser.add_argument('-u', '--uri', help='neo4j connection string', action='store')
+    parser.add_argument('-d', '--database', help='the neo4j database to query')
+    parser.add_argument('-g', '--graph', help='the <name> in <name>graph.py', action='store')
     parser.add_argument('-n', '--name', help='username for neo4j account',action='store')
-    parser.add_argument('-p', '--password', help='password for neo4j account', action='store')
-    parser.add_argument('-g', '--graphname', help='the name in namegraph.py', action='store')
     parser.add_argument('-o', '--output', help='directory to write the generated module to', action='store')
-
+    parser.add_argument('-p', '--password', help='password for neo4j account', action='store')
+    parser.add_argument('-u', '--uri', help='neo4j connection string', action='store')
+    
     args = parser.parse_args()
 
     # Update the graph object if command-line arguments were passed.
     if args.uri is not None and args.name is not None and args.password is not None:
-        graph['uri'] = args.uri
-        graph['username'] = args.name
-        graph['password'] = args.password
+        profile['uri'] = args.uri
+        profile['username'] = args.name
+        profile['password'] = args.password
+        profile['database'] = args.database
 
     # Default output is newgraph.py
-    graphname = args.graphname
+    graph = args.graph
 
     # If no output argument is passed, use the 
     output_directory = args.output if args.output is not None else 'generated_modules'
                       
-    print(f'Generating: {graphname}graph.py',
-          f'At: {graph["uri"]}',
-          f'Username: {graph["username"]}',
-          f'Password: {"*" * len(graph["password"])}',
+    print(f'Generating: {graph}graph.py',
+          f'At: {profile["uri"]}',
+          f'For database: {profile["database"]}'
+          f'Username: {profile["username"]}',
+          f'Password: {"*" * len(profile["password"])}',
           f'Generated module will be written to: {output_directory}/',
           sep='\n')
 
     # Generating the module with our neo4j-dev hardcoded. This instance contains
     # only sample data.
     generate_module(uri='bolt://localhost:7687', 
-                    graph=graphname, 
+                    graph=graph, 
                     output_directory=output_directory)
